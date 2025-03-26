0ea9d69cf140a913c5e474808995e3d346482bff
gubloise <gustavombloise5@gmail.com>
2025-03-26 15:04:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dropdowns = document.querySelectorAll(".dropdown");
-
-    dropdowns.forEach(dropdown => {
-        dropdown.addEventListener("mouseover", function() {
-            this.querySelector(".submenu").style.display = "block";
-        });
-
-        dropdown.addEventListener("mouseleave", function() {
-            this.querySelector(".submenu").style.display = "none";
-        });
-    });
-
-    // Mensagem no console para testar se o JS tá carregando
-    console.log("JavaScript carregado com sucesso!");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Neon-X está pronto!");
 });
